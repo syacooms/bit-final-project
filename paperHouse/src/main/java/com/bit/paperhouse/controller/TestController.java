b3202d765184a7ca209b4360b6a96877f4ff647b
drexqq <01037923530a@gmail.com>
2020-10-08 11:21:30 +0900

    [fix]

---
+++ paperHouse/src/main/java/com/bit/paperhouse/controller/TestController.java
@@ -0,0 +1,52 @@
+package com.bit.paperhouse.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bit.paperhouse.dto.UserDto;
+import com.bit.paperhouse.service.UserService;
+
+@Controller
+public class TestController {
+
+	@Autowired
+	UserService svc;
+
+	  // 메인 페이지
+    @GetMapping("/")
+    public String index() {
+        return "/index";
+    }
+
+    // 회원가입 페이지
+    @GetMapping("/user/join")
+    public String dispJoin() {
+        return "/join";
+    }
+
+    // 회원가입 처리
+    @PostMapping("/user/join")
+    public String execSignup(UserDto UserDto) {
+        svc.regi(UserDto);
+        return "redirect:/user/login";
+    }
+    
+    // 로그인 페이지
+    @GetMapping("/user/login")
+    public String dispLogin() {
+    	System.out.println("Login");
+        return "/login";
+    }
+    
+    // 로그인 결과 페이지
+    @GetMapping("/user/login/result")
+    public String dispLoginResult() {
+    	System.out.println("Login result");
+    	
+        return "/main";
+    }
+}
---
+++ paperHouse/src/main/java/com/bit/paperhouse/service/UserService.java
@@ -0,0 +1,34 @@
+package com.bit.paperhouse.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bit.paperhouse.dao.UserRepository;
+import com.bit.paperhouse.dto.UserDto;
+
+
+@Service
+@Transactional
+public class UserService {
+
+	@Autowired
+	UserRepository dao;
+
+	@Autowired
+	PasswordEncoder bCryptPasswordEncoder;
+
+
+	public void regi( UserDto dto ) {
+		
+		String rPwd = dto.getPwd();
+		String securityPwd = bCryptPasswordEncoder.encode(rPwd);
+		dto.setAuthority("ROLE_USER");
+		dto.setPwd(securityPwd);
+		dto.setNickName("화려한유니콘654");
+		 dao.regi(dto);
+	}
+
+
+}
