863a53c6d3e3945bf3f34585b384217b243bf7e6
syacooms <syacooms@gmail.com>
2020-10-12 10:28:04 +0900

    [java] dto fix

--- paperHouse/src/main/java/com/bit/paperhouse/dto/WriterDto.java
+++ paperHouse/src/main/java/com/bit/paperhouse/dto/WriterDto.java
@@ -16,11 +16,7 @@ public class WriterDto implements Serializable {
 
 	private int userSeq;
 	private int writerSeq;
-<<<<<<< HEAD
 	private String name;
-=======
-	private String writerName;
->>>>>>> bc5828ca348ad30ca6886c8f1f9e12f96af0b63a
 	private String email;
 	private String intro;
 	private String fileOriginalName;
@@ -30,9 +26,5 @@ public class WriterDto implements Serializable {
 	//user subscribe dto 대신 writerDto 사용
 	private String startDate;
 	private String endDate;
-<<<<<<< HEAD
-=======
-	
->>>>>>> bc5828ca348ad30ca6886c8f1f9e12f96af0b63a
 	
 }
