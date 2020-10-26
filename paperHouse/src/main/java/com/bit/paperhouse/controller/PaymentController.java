ca24b5abf463d2af886c9dcd7f65275b35a3bf57
skjfjd <gembce@naver.com>
2020-10-26 13:55:29 +0900

    qna detail 수정

--- paperHouse/src/main/java/com/bit/paperhouse/controller/PaymentController.java
+++ paperHouse/src/main/java/com/bit/paperhouse/controller/PaymentController.java
@@ -2,10 +2,7 @@ package com.bit.paperhouse.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import com.bit.paperhouse.dto.PaymentDto;
 import com.bit.paperhouse.dto.UserSubscribeDto;
--- paperHouse/src/main/java/com/bit/paperhouse/controller/QnaReplyController.java
+++ paperHouse/src/main/java/com/bit/paperhouse/controller/QnaReplyController.java
@@ -1,11 +1,14 @@
 package com.bit.paperhouse.controller;
 
+import com.bit.paperhouse.dto.QnaDto;
 import com.bit.paperhouse.dto.QnaReplyDto;
 import com.bit.paperhouse.service.QnaReplyService;
 import com.bit.paperhouse.service.QnaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -21,26 +24,24 @@ public class QnaReplyController {
 
     @ResponseBody
     @PostMapping("/qna/replyList")
-    public List<QnaReplyDto> replyList(int qnaSeq ){
+    public List<QnaReplyDto> replyList(@RequestParam("qnaSeq") int qnaSeq , Model model){
 
      List<QnaReplyDto> list =  service.replyList(qnaSeq);
-
+     model.addAttribute("re",list);
 
       return list;
     }
     @ResponseBody
     @PostMapping("/qna/replyInsert")
-    public String  replyInsert(QnaReplyDto dto){
+    public List<QnaReplyDto>  replyInsert(QnaReplyDto dto , Model model){
         System.out.println(dto.toString());
         int qnaSeq = dto.getQnaSeq();
         service.replyInsert(dto);
-       qnaService.qnaStaus(qnaSeq);
+        qnaService.qnaStaus(qnaSeq);
 
-     String msg = "새로고침을 하시면 답변이 보여요";
-
-     return  msg;
-
-
+        List<QnaReplyDto> list =  service.replyList(qnaSeq);
+        model.addAttribute("reply" , list);
+       return  list;
     }
 
 }
